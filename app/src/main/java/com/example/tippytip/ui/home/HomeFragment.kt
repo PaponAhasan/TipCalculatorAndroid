e92017284c755f8a6b98830dda8060e3d6e08cc1
PaponAhasan <ahasan.papon@gmail.com>
2023-01-15 16:37:05 +0600

    tippytip app

--- app/src/main/java/com/example/tippytip/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/tippytip/ui/home/HomeFragment.kt
@@ -1,20 +1,24 @@
 package com.example.tippytip.ui.home
 
 import android.animation.ArgbEvaluator
+import android.graphics.Color
 import android.os.Bundle
 import android.text.Editable
+import android.text.InputFilter
 import android.text.TextWatcher
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.SeekBar
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.tippytip.R
 import com.example.tippytip.databinding.FragmentHomeBinding
+import kotlin.math.roundToInt
 
 private const val TAG = "HomeFragment"
 private const val INITIAL_TIP_PERCENT = 15
@@ -30,11 +34,6 @@ class HomeFragment : Fragment() {
         ViewModelProvider(this)[HomeViewModel::class.java]
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
-
-//        val textView: TextView = binding.textHome
-//        homeViewModel.text.observe(viewLifecycleOwner) {
-//            textView.text = it
-//        }
 
         binding.seekBarTip.progress = INITIAL_TIP_PERCENT
         binding.tvTipPercentLabel.text = "$INITIAL_TIP_PERCENT%"
@@ -63,7 +62,45 @@ class HomeFragment : Fragment() {
             }
 
         })
+        binding.etNumberOfPerson.addTextChangedListener(object :TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                val inputFilter = InputFilter { source, start, end, dest, dstart, dend ->
+                    if (dstart == 0 && source.startsWith("0")) {
+                        ""
+                    } else source
+                }
+                binding.etNumberOfPerson.filters = arrayOf(inputFilter)
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                computeTipAndTotal()
+            }
+
+        })
+        binding.checkBoxRoundAmount.setOnCheckedChangeListener{buttonView, isChecked ->
+            if (isChecked) {
+                // The checkbox is checked
+                computeRoundTipAndTotal()
+                Toast.makeText(context, "Round up to tip and amount", Toast.LENGTH_SHORT).show()
+            } else {
+                // The checkbox is not checked
+                computeTipAndTotal()
+                Toast.makeText(context, "Unchecked", Toast.LENGTH_SHORT).show()
+            }
+        }
+        binding.btnSave.setOnClickListener{
+            if(binding.tvTipAmount.text.isNotEmpty()){
+                Toast.makeText(context, "Saved your result", Toast.LENGTH_SHORT).show()
+            }
+        }
         return binding.root
+    }
+
+    private fun computeRoundTipAndTotal() {
+        binding.tvTipAmount.text = binding.tvTipAmount.text.toString().toDouble().roundToInt().toString()
+        binding.tvTotalAmount.text = binding.tvTotalAmount.text.toString().toDouble().roundToInt().toString()
     }
 
     private fun updateTipDescription(tipPercent: Int) {
@@ -88,17 +125,18 @@ class HomeFragment : Fragment() {
 
     private fun computeTipAndTotal() {
         // Solved first issue
-        if(binding.etBaseAmount.text.isEmpty()){
+        if(binding.etBaseAmount.text.isEmpty() or binding.etNumberOfPerson.text.isEmpty()){
             binding.tvTipAmount.text = ""
             binding.tvTotalAmount.text = ""
             return
         }
         // 1. Get the value of the base and tip percent
         val baseAmount = binding.etBaseAmount.text.toString().toDouble()
+        val numberOfPerson = binding.etNumberOfPerson.text.toString().toInt()
         val tipPercent = binding.seekBarTip.progress
         // 2. Compute the tip and total
-        val tipAmount = baseAmount * tipPercent / 100
-        val totalAmount = baseAmount + tipAmount
+        val tipAmount = (baseAmount * tipPercent / 100) / numberOfPerson
+        val totalAmount = (baseAmount + tipAmount) / numberOfPerson
         // 3. Update the UI
         binding.tvTipAmount.text = "%.2f".format(tipAmount)
         binding.tvTotalAmount.text = "%.2f".format(totalAmount)
@@ -110,6 +148,14 @@ class HomeFragment : Fragment() {
 
           02. Formatting the output of the tip and total amount, when base amount decimal (100.9) total
            amount very long and unwieldy
+         */
+
+        /*
+            Answer:
+            Tip Each Person:	$ 1.51
+            Total Each Person:	$ 51.96
+            Tip Total:	$ 3.03
+            Total (Bill + Tip):	$ 103.93
          */
     }
 
